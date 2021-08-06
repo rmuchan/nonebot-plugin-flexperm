db4db650ba25e0152b4cd04d307d7ba69102e6c0
Muchan <liuzh1773@buaa.edu.cn>
2021-08-06 12:20:55 +0800

    检查权限过程中的调试输出

--- permman/check.py
+++ permman/check.py
@@ -1,53 +1,60 @@
-from typing import Optional, Iterable
+from typing import Iterable
 
+from nonebot import logger
 from nonebot.adapters import Bot, Event
 from nonebot.adapters.cqhttp import PrivateMessageEvent, GroupMessageEvent
 
-from .core import get, CheckResult
+from .config import c
+from .core import get, CheckResult, PermissionGroup
 
 
 def check(bot: Bot, event: Event, perm: str) -> bool:
-    for r in do_check(bot, event, perm):
+    if c.permman_debug_check:
+        logger.debug('Checking {}', perm)
+    for group in iterate_groups(bot, event):
+        r = group.check(perm)
+        if c.permman_debug_check:
+            logger.debug('Got {} from {}', r, group)
         if r is not None:
             return r == CheckResult.ALLOW
 
     return False
 
 
-def do_check(bot: Bot, event: Event, perm: str) -> Iterable[Optional[CheckResult]]:
+def iterate_groups(bot: Bot, event: Event) -> Iterable[PermissionGroup]:
     # 特定用户
     user = getattr(event, 'user_id', None) or event.get_user_id()
     group, _ = get('user', user)
-    yield group.check(perm)
+    yield group
 
     # Bot超级用户
     if event.get_user_id() in bot.config.superusers:
         group, _ = get('global', 'superuser')
-        yield group.check(perm)
+        yield group
 
     # 所有用户
     group, _ = get('global', 'anyone')
-    yield group.check(perm)
+    yield group
 
     # 群组
     if isinstance(event, GroupMessageEvent):
         # 用户在群组内的身份
         if event.sender.role == 'admin':
             group, _ = get('global', 'group_admin')
-            yield group.check(perm)
+            yield group
         elif event.sender.role == 'owner':
             group, _ = get('global', 'group_owner')
-            yield group.check(perm)
+            yield group
 
         # 特定群组
         group, _ = get('group', event.group_id)
-        yield group.check(perm)
+        yield group
 
         # 所有群组
         group, _ = get('global', 'group')
-        yield group.check(perm)
+        yield group
 
     # 私聊
     if isinstance(event, PrivateMessageEvent):
         group, _ = get('global', 'private')
-        yield group.check(perm)
+        yield group
--- permman/config.py
+++ permman/config.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 
 class Config(BaseModel):
     permman_base: Path = Path('permissions')
+    permman_debug_check: bool = False
 
 
 c = Config(**nonebot.get_driver().config.dict())
